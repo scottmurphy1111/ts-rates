15cd32ed5d24c67d1677a429daeeb27270e12339
Scott Murphy <scottmurphy1111@gmail.com>
2024-02-18 07:55:59 -0500

    easier fix sorting

--- src/routes/(app)/output/RatesOutput.svelte
+++ src/routes/(app)/output/RatesOutput.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import type { RatesheetWithIncludes } from '$lib/types/types';
 	import type { Markup } from '@prisma/client';
-	import VocationalRates from './VocationalRates.svelte';
 
 	export let ratesheetData = {} as RatesheetWithIncludes;
 	export let markups = [] as Markup[];
@@ -18,6 +17,11 @@
 		{} as Record<string, { markup: string }>
 	);
 
+	const termHeaderVocation = () =>
+		ratesheetData.lowMileageCutoff?.toLowerCase() === 'n/a'
+			? `Term / Type  (All years / Unlimited Miles)`
+			: `Term / Type (All years / 0 - ${ratesheetData.lowMileageCutoff}k miles)`;
+
 	$: rateColHeaders = [
 		'Term / Mileage Limits',
 		`${new Date().getFullYear() - 4} &amp; Newer <br /> 0-${ratesheetData.lowMileageCutoff}K`,
@@ -28,8 +32,13 @@
 		'Aggregate Limit'
 	];
 
+	$: rateColHeadersVocational = [termHeaderVocation(), 'Cost', 'Deductible', 'Aggregate Limit'];
+
 	const ratesHeadersCount = (node: HTMLDivElement) => {
 		node.style.gridTemplateColumns = `repeat(${rateColHeaders.length}, minmax(min-content, auto))`;
+	};
+	const ratesHeadersCountVocational = (node: HTMLDivElement) => {
+		node.style.gridTemplateColumns = `repeat(${rateColHeadersVocational.length}, minmax(min-content, auto))`;
 	};
 
 	const calculateMarkup = (termValue: string, cost: string) => {
@@ -43,7 +52,53 @@
 <div class="card shadow-xl p-8 mb-8">
 	<!-- Vocational -->
 	{#if ratesheetData?.isVocational}
-		<VocationalRates {ratesheetData} {markups} {color} />
+		<div
+			class={`grid justify-items-center place-items-end grid-flow-dense auto-rows-auto bg-${color}-500`}
+			style="margin-inline: auto; margin-block: auto; gap: 2px;"
+			use:ratesHeadersCountVocational
+		>
+			{#each rateColHeadersVocational as header}
+				<div
+					class="flex flex-col text-base font-extrabold bg-white min-w-full px-2 pb-2 h-full justify-end"
+				>
+					{@html header}
+				</div>
+			{/each}
+			{#if ratesheetData?.rows}
+				{#each ratesheetData.rows.sort((a, b) => (String(b.truckType) > String(a.truckType) ? 1 : String(b.truckType) < String(a.truckType) ? -1 : 0) || Number(a.costNewerLowMiles) - Number(b.costNewerLowMiles)) as row, k}
+					{#each Object.entries(row) as [key, value], i}
+						{#if (key === 'costNewerLowMiles' || key === 'deductible' || key === 'aggregateLimit') && value !== null}
+							<div
+								class="inline-flex items-baseline text-base font-semibold bg-white min-w-full h-full p-2"
+							>
+								${new Intl.NumberFormat('en-US', {
+									style: 'decimal',
+									currency: 'USD'
+								}).format(
+									key.startsWith('cost') ? calculateMarkup(row.termValue, value) : Number(value)
+								)}
+							</div>
+						{/if}
+						{#if key === 'termValue'}
+							<div
+								class="inline-flex items-baseline text-base font-semibold bg-white min-w-full h-full p-2"
+							>
+								{value}
+								{row['termUnit']} / {(
+									row['truckType']?.charAt(0).toUpperCase() + (row['truckType'] ?? '').slice(1)
+								).replace(/([A-Z])/g, ' $1')}
+							</div>
+						{/if}
+
+						<!-- {#if i !== 0}
+							key {key}<br />
+							value {value} <br />
+							i {i} <br />
+						{/if} -->
+					{/each}
+				{/each}
+			{/if}
+		</div>
 
 		<!-- Non Vocational -->
 	{:else}
